511af8efa34c7f878d886ce69d86e01915b5647b
Eugene Lamnek <eugene@geoit.com.au>
2023-08-10 10:15:52 +0930

    ballast trim

--- Harmonia2/control/syringe_ballast.cpp
+++ Harmonia2/control/syringe_ballast.cpp
@@ -5,6 +5,9 @@
 #include "syringe_ballast.h"
 #include <Motoron.h>
 #include <motoron_protocol.h>
+#include <PID_v1.h>
+//https://playground.arduino.cc/Code/PIDLibaryBasicExample/
+
 
 //motor controller bays
 #define forBalMC 1
@@ -18,6 +21,20 @@ MotoronI2C BallastMC;
 
 int m_intFwdMotorSpeed;
 int m_intAftMotorSpeed;
+
+
+//define the PIDs to drecctly control the position of the plungers
+//setpoints will be supplied to this module as an input
+//PID inputs will be plunger position (from leonardo encoder reading)
+//PID outputs will be a speed factor
+double fwdBalSetpoint, fwdBalInput, fwdBalOutput;
+double aftBalSetpoint, aftBalInput, aftBalOutput;
+double KpBal = 8, KiBal = 0, KdBal = 0.1;
+PID fwdBalPID(&fwdBalInput, &fwdBalOutput, &fwdBalSetpoint, KpBal, KiBal, KdBal, DIRECT);
+PID aftBalPID(&aftBalInput, &aftBalOutput, &aftBalSetpoint, KpBal, KiBal, KdBal, DIRECT);
+
+boolean m_blnFwdInitialising;
+boolean m_blnAftInitialising;
 
 void ballast_init() {
 
@@ -46,7 +63,16 @@ void ballast_init() {
 	BallastMC.clearMotorFault();
 	resetBallast();
 
+	m_blnFwdInitialising = true;
+	m_blnAftInitialising = true;
 
+}
+
+void setpoint_follow(double fwdPosSetpoint,double aftPosSetpoint) {
+	if (m_blnFwdInitialising) {
+		//syringe is still initialising
+
+	}
 }
 void command_ballast(String strCommand, int intValue) {
 
@@ -75,7 +101,7 @@ int32_t read_aft_ballast_pos() {
 	return readBallastPos(aftBalAddr);	
 }
 
-
+//sends a reset command (*R) to both leonardos to tell them to set positions to -100
 void resetBallast(void) {
 	Wire2.beginTransmission(fwdBalAddr);
 	Wire2.write("*R");
