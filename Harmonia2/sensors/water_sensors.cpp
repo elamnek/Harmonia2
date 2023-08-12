fdf522167e7f3e19d285e56e601f7d82cffe59d9
Eugene Lamnek <eugene@geoit.com.au>
2023-08-12 17:28:07 +0930

    water sensor fix

--- Harmonia2/sensors/water_sensors.cpp
+++ Harmonia2/sensors/water_sensors.cpp
@@ -4,8 +4,8 @@
 
 #include "water_sensors.h"
 
-int m_intFwdWaterSensorPin = 16;
-int m_intAftWaterSensorPin = 14;
+int m_intFwdWaterSensorPin = 40;
+int m_intAftWaterSensorPin = 38;
 
 void init_watersensors() {
 	pinMode(m_intFwdWaterSensorPin, INPUT);
@@ -13,14 +13,14 @@ void init_watersensors() {
 }
 
 int fwd_leak_check() {
-	//normal state of pins = 1 changes to 0 if water touches sensor track
+	//normal state of pins = 0 changes to 1 if water touches sensor track
 	if (digitalRead(m_intFwdWaterSensorPin) == 1) {
 		return 1;
 	}
 	return 0;
 }
 int aft_leak_check() {
-	//normal state of pins = 1 changes to 0 if water touches sensor track
+	//normal state of pins = 0 changes to 1 if water touches sensor track
 	if (digitalRead(m_intAftWaterSensorPin) == 1) {
 		return 1;
 	}
@@ -29,17 +29,17 @@ int aft_leak_check() {
 
 
 //analog reads for testing only - not used currently
-boolean leak_detected2() {
-
-	if (analogRead(m_intFwdWaterSensorPin) < 80) {
-		//|| digitalRead(m_intAftWaterSensorPin) == 0
-
-		return true;
-	}
-	return false;
-}
-uint8_t leak_read() {
-	return analogRead(m_intFwdWaterSensorPin);
-}
+//boolean leak_detected2() {
+//
+//	if (analogRead(m_intFwdWaterSensorPin) < 80) {
+//		//|| digitalRead(m_intAftWaterSensorPin) == 0
+//
+//		return true;
+//	}
+//	return false;
+//}
+//uint8_t leak_read() {
+//	return analogRead(m_intFwdWaterSensorPin);
+//}
 
 
